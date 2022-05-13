98926aa83336fa2f89bb4dfb40ae41dcdd386d89
hezhihang <hezhihang@baidu.com>
2022-05-13 15:12:23 +0800

    修改 svg to component 逻辑

--- build/build-component.ts
+++ build/build-component.ts
@@ -1,46 +1,16 @@
 import fs from "fs";
 import path from "path";
 import prettier from "prettier";
-import { convertCode, toCamelCase } from "./utils";
 
 const svgSourcePath = path.resolve(__dirname, "../source");
 
 const allIconMap: Record<string, string> = {};
 
-const traverseFile = (dirPath: string) => {
-  const files = fs.readdirSync(dirPath, { withFileTypes: true });
-  files.forEach((file) => {
-    if (file.isFile()) {
-      if (allIconMap[file.name]) {
-        console.log("\x1b[33m 存在重复名称图标： \x1b[0m", file.name);
-      }
-      if (path.extname(file.name) !== ".svg") {
-        console.log("\x1b[33m 非svg图标： \x1b[0m", file.name);
-      } else {
-        allIconMap[file.name] = path.join(dirPath, file.name);
-      }
-    } else if (file.isDirectory()) {
-      traverseFile(path.join(dirPath, file.name));
-    }
-  });
-};
-
-const compile = (type: "react" | "vue") => {
+const compile = (type: "react") => {
   const iconsDir = path.resolve(__dirname, `../packages/${type}/src/icons`);
   if (!fs.existsSync(iconsDir)) {
     fs.mkdirSync(iconsDir);
   }
-
-  const generateCode = (svgFilePath: string) => {
-    const content = fs.readFileSync(svgFilePath, { encoding: "utf8" });
-    const fileName = path.basename(svgFilePath, ".svg");
-    const extname = ".tsx";
-    fs.writeFileSync(
-      path.join(iconsDir, `${toCamelCase(fileName)}${extname}`),
-      convertCode(type, fileName, content),
-      { encoding: "utf8" }
-    );
-  };
 
   const exportCodeIntoMapFile = () => {
     const fileNames = fs.readdirSync(iconsDir);
@@ -55,7 +25,7 @@ const compile = (type: "react" | "vue") => {
       fileContent += `export {default as ${name}} from './icons/${name}';\n`;
     });
     fs.writeFileSync(
-      path.resolve(iconsDir, "../map.ts"),
+      path.resolve(iconsDir, "../index.ts"),
       prettier.format(fileContent, {
         parser: "babel",
         tabWidth: 2
@@ -64,10 +34,7 @@ const compile = (type: "react" | "vue") => {
     );
   };
 
-  Object.values(allIconMap).forEach((filePath) => generateCode(filePath));
-
   exportCodeIntoMapFile();
 };
 
-traverseFile(svgSourcePath);
 compile("react");
